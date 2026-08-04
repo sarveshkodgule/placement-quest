69b2e1a3793095a0e368d2e7da7433bb0f13b502
Sarvesh Kodgule <sarveshkodgule17@gmail.com>
2026-08-04 13:09:21 +0530

    apti district ui fix

--- Frontend/src/games/AptitudeDistrict.jsx
+++ Frontend/src/games/AptitudeDistrict.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+    import React, { useState, useEffect, useRef } from 'react';
 import ConfettiEffect from '../components/ConfettiEffect';
 import { usePlayerStore } from '../store/usePlayerStore';
 import { 
